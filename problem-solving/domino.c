3b72ec8120b63d4bd39ca71f8b2a7d4afe1c6146
siddiq <siddiq.aiub@gmail.com>
2022-10-22 09:36:11 +0600

    domino calculation

--- problem-solving/domino.c
+++ problem-solving/domino.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
 
 int main(){
-    printf("hello");
+    int m,n,ans;
+    scanf("%d%d",&n,&m);
+    if(m==1 && n==1){
+        ans = 0;
+    }else if((n==1 && m>1)){
+        ans = m-1;
+    }else if( (m==1 && n>1)){
+        ans = n-1;
+    }
+    else{
+        ans = (m-1)*n;
+    }
+    printf("%d",ans);
 }
