e3c0a08183324bc69fa9bc4628d9b1d71082a017
siddiq <siddiq.aiub@gmail.com>
2022-11-02 19:01:08 +0600

    domino

--- problem-solving/domino.c
+++ problem-solving/domino.c
@@ -2,13 +2,13 @@
 
 int main(){
     int m,n,ans;
-    scanf("%d%d",&n,&m);
+    scanf("%d%d",&m,&n);
     if(m==1 && n==1){
         ans = 0;
-    }else if((n==1 && m>1)){
-        ans = m-1;
-    }else if( (m==1 && n>1)){
+    }else if((n>1 && m==1)){
         ans = n-1;
+    }else if( (n==1 && m>1)){
+        ans = m-1;
     }
     else{
         ans = (m-1)*n;
